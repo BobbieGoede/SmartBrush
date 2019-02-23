e08aac3dcedf96d0c814c13b9e343a1993e47197
Bobbie Goede <bobbiegoede@gmail.com>
2019-02-23 15:50:21 +0100

    First commit

---
+++ Editor/SmartBrush.cs
@@ -0,0 +1,77 @@
+﻿using System.Reflection;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.Tilemaps;
+using System;
+
+[CustomGridBrush(false, false, false, "SmartBrush")]
+public class SmartBrush : GridBrush {
+	public override void Paint(GridLayout grid, GameObject brushTarget, Vector3Int position) {
+		base.Paint(grid, brushTarget, position);
+	}
+
+	public override void Erase(GridLayout grid, GameObject brushTarget, Vector3Int position) {
+		base.Erase(grid, brushTarget, position);
+	}
+
+	public override void FloodFill(GridLayout grid, GameObject brushTarget, Vector3Int position) {
+		base.FloodFill(grid, brushTarget, position);
+	}
+
+	public override void BoxFill(GridLayout gridLayout, GameObject brushTarget, BoundsInt position) {
+		base.BoxFill(gridLayout, brushTarget, position);
+	}
+
+	public override void Pick(GridLayout gridLayout, GameObject brushTarget, BoundsInt position, Vector3Int pickStart) {
+		Tilemap component = brushTarget.GetComponent<Tilemap>();
+		if(component == null) return;
+
+		base.Pick(gridLayout, brushTarget, position, pickStart);
+
+		foreach(Vector3Int tilePosition in position.allPositionsWithin) {
+			if(component.GetTile(tilePosition) == null) continue;
+			TilemapExtensions.AutoSelectPaintTarget();
+			return;
+		}
+	}
+}
+
+[CustomEditor(typeof(SmartBrush))]
+public class SmartBrushEditor : GridBrushEditor {
+	protected override void OnEnable() {
+		base.OnEnable();
+		TilemapExtensions.FlushCache = true;
+	}
+
+	protected override void OnDisable() {
+		base.OnDisable();
+		TilemapExtensions.FlushCache = true;
+	}
+
+	public override GameObject[] validTargets {
+		get {
+			StageHandle currentStageHandle = StageUtility.GetCurrentStageHandle();
+			if(brush.cells.Count() == 0) {
+				Debug.LogWarning("No cells selected, can't set active Tilemap");
+				return null;
+			}
+
+			return currentStageHandle
+				.FindComponentsOfType<Tilemap>()
+				.Where(
+					x => x.gameObject.scene.isLoaded &&
+					x.gameObject.activeInHierarchy &&
+					Array.Exists(brush.cells, c =>
+						c.tile != null &&
+						c.tile.name == x.gameObject.name
+					)
+				)
+				.Select(x => x.gameObject)
+				.ToArray();
+		}
+	}
+}
---
+++ Editor/TilemapExtensions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Reflection;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public static class TilemapExtensions {
+	public static bool FlushCache = true;
+	static Type gridPaintingStateType;
+	static MethodInfo autoSelectPaintTarget;
+	static object instance;
+	static FieldInfo flushPaintTargetCache;
+	static PropertyInfo scenePaintTarget;
+
+	static void CacheReflectionFields() {
+		gridPaintingStateType = Type.GetType("UnityEditor.GridPaintingState,UnityEditor");
+		instance = gridPaintingStateType
+			.GetProperty("instance", BindingFlags.Public | BindingFlags.Static | BindingFlags.FlattenHierarchy)
+			.GetValue(gridPaintingStateType, null);
+		flushPaintTargetCache = gridPaintingStateType.GetField("m_FlushPaintTargetCache", BindingFlags.NonPublic | BindingFlags.Instance);
+		autoSelectPaintTarget = gridPaintingStateType.GetMethod("AutoSelectPaintTarget", BindingFlags.Public | BindingFlags.Static);
+		scenePaintTarget = gridPaintingStateType.GetProperty("scenePaintTarget", BindingFlags.Public | BindingFlags.Static);
+		FlushCache = false;
+	}
+
+	public static void AutoSelectPaintTarget() {
+		if(FlushCache) CacheReflectionFields();
+
+		flushPaintTargetCache.SetValue(instance, true);
+		autoSelectPaintTarget.Invoke(null, null);
+
+		Selection.activeGameObject = (GameObject)scenePaintTarget.GetValue(instance);
+	}
+}
